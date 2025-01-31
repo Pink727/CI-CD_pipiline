d7b611ac6b96e0d5a9b0efd84ecda515ad2e36e6
Pink727 <doc72789@gmail.com>
2025-01-30 17:27:44 -0800

    Add vite-plugin-node-polyfills to support Node.js polyfills in Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import NodePolyfills from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    NodePolyfills({
+      // To include specific polyfills, you can configure here
+      crypto: true,
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
