492622e46c5293d4a1015501d1c49fc2ee243be2
Pink727 <doc72789@gmail.com>
2025-01-30 16:27:29 -0800

    Enhance database seeding by adding model existence check and refactoring file reading for questions

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,12 +1,15 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
+import fs from 'fs/promises';
+import path from 'path';
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+const pythonQuestionsPath = path.resolve(__dirname, './pythonQuestions.json');
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
 
+  const pythonQuestions = JSON.parse(await fs.readFile(pythonQuestionsPath, 'utf-8'));
   await Question.insertMany(pythonQuestions);
 
   console.log('Questions seeded!');
