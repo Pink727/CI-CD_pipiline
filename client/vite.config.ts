68c0b4e4ee953b1b7a9ff17d3c77c8b1c06364ac
Pink727 <doc72789@gmail.com>
2025-01-30 17:35:42 -0800

    Remove vite-plugin-node-polyfills from dependencies and Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import NodePolyfills from 'vite-plugin-node-polyfills';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    NodePolyfills({
-      // To include specific polyfills, you can configure here
-      crypto: true,
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
