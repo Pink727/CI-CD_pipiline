30cbd82329d8a1bbef99014fb2ee0c91dd81a289
Pink727 <doc72789@gmail.com>
2025-01-30 16:31:12 -0800

    Refactor cleanDb function to streamline model existence check and collection listing

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,9 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    const model = models[modelName];
-    if (!model) {
-      throw new Error(`Model ${modelName} does not exist`);
-    }
-
-    let modelExists = await model.db.db.listCollections({
+    let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
-    }).toArray();
+    }).toArray()
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
